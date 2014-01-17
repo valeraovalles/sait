91532118727dabb381adc381ac6d2d63e027d685
root <root@jvalera-pc.(none)>
2014-01-17 10:20:43 -0430

    anios en txt de nomina

--- src/Frontend/NominaBundle/Controller/DefaultController.php
+++ src/Frontend/NominaBundle/Controller/DefaultController.php
@@ -17,7 +17,7 @@ class DefaultController extends Controller
     public function formalimentacionAction(){
 
     	$tn=array('0'=>'Seleccione...', '21'=>'Trabajadores e','25'=>'Trabajadores o','14'=>'Trabajadores an','24'=>'Contratados','32'=>'Comision de servicios','31'=>'Convenios internacionales','41'=>'Jubilados y pensionados');
-    	$anio=array('0'=>'Seleccione...', '2012'=>'2012','2013'=>'2013');
+    	$anio=array('0'=>'Seleccione...', date("Y")-2=>date("Y")-2,date("Y")-1=>date("Y")-1,date("Y")=>date("Y"));
     	$mes=array('0'=>'Seleccione...', '1'=>'Enero','2'=>'Febrero','3'=>'Marzo','4'=>'Abril','5'=>'Mayo','6'=>'Junio','7'=>'Julio','8'=>'Agosto','9'=>'Septiembre','10'=>'Octubre','11'=>'Noviembre','12'=>'Diciembre');
 		$form = $this->createFormBuilder()
 		        ->add('tiponomina', 'choice', array(
